ab0307dbf6e8ce09c6c6a40382d09eae83a63367
Patrick Lodder <patrick.lodder@intellibiz.org>
2015-03-26 14:12:02 +0100

    refactor

--- index.js
+++ index.js
@@ -1,27 +1,2 @@
-var _ = require('lodash');
-var requestp = require('request-promise');
-
-var shapeshift = module.exports = {};
-var url = 'http://shapeshift.io';
-var defaults = {
-	json: true,
-	resolveWithFullResponse: true
-};
-
-//todo add support for callbacks instead of promises with options arg
-
-function get(url, opts){
-	opts = opts || {};
-
-	return requestp(_.extend(defaults, opts, {
-		url: url
-	}));
-}
-
-shapeshift.getRate = function(pair){
-	return get(url + '/rate/' + pair);
-};
-
-shapeshift.getLimit = function(pair){
-	return get(url + '/limit/' + pair);
-};
+// retained for compat
+var index = module.exports = require('./lib/shapeshift');
